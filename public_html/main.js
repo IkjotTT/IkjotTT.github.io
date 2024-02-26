a6d81f47fa5c2e61c68bf569cb8fe4033196d11e
IkjotTT <ikjot.titrie@torontomu.ca>
2024-02-26 17:35:45 -0500

    Create main.js

---
+++ public_html/main.js
@@ -0,0 +1,23 @@
+// this is a function used to prompt the user for their name, the name is stored into name variable and then displayed on screen on homepage
+function promptName() {
+    var name = prompt("Please enter your name:");
+    if (name != null && name != "") {
+        document.getElementById("name").innerHTML = "Hello, " + name + "!";
+        document.getElementById("nameButton").style.display = "none";
+    }
+}
+
+function enlargeImage(img) { // a function that takes in the image, it then changes the size of the image based on the CSS styling code (increases it size while also having a slow transition)
+    img.classList.toggle('enlarged');
+}
+
+// A function that adds a shadowy effect when the user hovers off the specific dropdown menu item
+function Shadow(element) {
+    element.style.boxShadow = '2px 2px 5px rgba(0, 0, 0, 0.1)';
+}
+
+// A function to darken background color with a tone of grey to make it seem that the user has already hovered/seen this element
+function darkBack(element) {
+    element.classList.add('mouseout-dark');
+
+}
