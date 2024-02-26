9c135a4d1c7b0a1b07337c45b8500728b324a915
IkjotTT <ikjot.titrie@torontomu.ca>
2024-02-26 03:28:29 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-alert('Test')
+function promptForName() {
+    var name = prompt("Please enter your name:");
+    if (name != null && name != "") {
+        document.getElementById("username").innerHTML = "Hello, " + name + "!";
+    }
+}
