43bbf7983e5992486e8d3ed9b3a23e8a0a2909b3
IkjotTT <ikjot.titrie@torontomu.ca>
2024-02-26 18:58:27 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,21 @@
+function promptName() { // this is a function used to prompt the user for their name, the name is stored into name variable and then displayed on screen on homepage
+    var name = prompt("Please enter your name:"); // prompts the user to input name
+    if (name != null && name != "") { //checks to see if input is blank or null
+        document.getElementById("name").innerHTML = "Hello, " + name + "!"; //if true it will display a hello message with the name of the individual
+        document.getElementById("nameButton").style.display = "none"; //removes the button to input name 
+    }
+}
+
+function enlargeImage(img) { // a function that takes in the image, it then changes the size of the image based on the CSS styling code (increases it size while also having a slow transition)
+    img.classList.toggle('enlarged'); //toggles the enlarged class 
+}
+
+
+function Shadow(element) { //this function takes a parameter and then applies a shadowy effect to 
+    element.style.boxShadow = '2px 2px 5px rgba(0, 0, 0, 0.1)';
+}
+
+function darkBack(element) { //function takes in a parameter as well as applies a darkened background color after the user has moved the mouse off the navigation element
+    element.classList.add('mouseout-dark');
+
+}
