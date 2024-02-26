d39158298e702500e8562fabc77c3bcbce92022a
IkjotTT <ikjot.titrie@torontomu.ca>
2024-02-26 15:44:49 -0500

    Update main.js

--- main.js
+++ main.js
@@ -10,6 +10,22 @@ function promptName() {
      }
 }
 
-function enlargeImage(img) {
+function enlargeImage(img) { // a function that takes in the image, it togglest the class englarged which contains image resizing code
     img.classList.toggle('enlarged');
 }
+
+function showDropdown() {
+    document.querySelector('.dropdown-content').style.display = 'block';
+  }
+  
+  function hideDropdown() {
+    document.querySelector('.dropdown-content').style.display = 'none';
+  }
+  
+  function showSubmenu() {
+    document.querySelector('.submenu-content').style.display = 'block';
+  }
+  
+  function hideSubmenu() {
+    document.querySelector('.submenu-content').style.display = 'none';
+  }
