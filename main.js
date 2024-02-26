bf5517d1a7e335a9058f0e6ce0b4bfa78a70ba6b
IkjotTT <ikjot.titrie@torontomu.ca>
2024-02-26 03:58:31 -0500

    Update main.js

--- main.js
+++ main.js
@@ -5,12 +5,3 @@ function promptForName() {
         document.getElementById("nameButton").style.display = "none";
     }
 }
-
-function navigateToPage(page) {
-    document.getElementById("content").innerHTML = "<h2>You clicked on " + page + "</h2>";
-}
-
-
-function changeBackgroundColor(color) {
-    document.body.style.backgroundColor = color;
-}
