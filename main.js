764ec79003f72210e083a1f72ae8cb94f931ee5d
IkjotTT <ikjot.titrie@torontomu.ca>
2024-02-26 12:06:17 -0500

    Update main.js

--- main.js
+++ main.js
@@ -6,12 +6,42 @@ function promptForName() {
     }
 }
 
-function toggleDropdown(element) {
-    const dropdownMenu = element.querySelector('.dropdown-menu');
-    if (dropdownMenu.style.display === 'none' || dropdownMenu.style.display === '') {
-      dropdownMenu.style.display = 'block';
+// Task a: Create a button that prompts the user to type their name and display it in an HTML element
+
+function promptForName() {
+    var name = prompt("Please enter your name:");
+    var outputElement = document.getElementById("nameOutput");
+    if (name !== null && name !== "") {
+        outputElement.innerHTML = "Hello, " + name + "!";
     } else {
-      dropdownMenu.style.display = 'none';
+        outputElement.innerHTML = "Hello, guest!";
     }
-  }
-  
+}
+
+// Task b: Make a clickable image that enlarges when clicked
+
+function enlargeImage() {
+    var image = document.getElementById("myImage");
+    image.style.width = "200px"; // adjust the width as desired
+}
+
+// Task c: Create a customized navigation menu
+
+function toggleMenu() {
+    var menu = document.getElementById("menuItems");
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "block";
+    }
+}
+
+// Task d: Add onmouseover/onmouseout events for navigation items
+
+function highlightItem(item) {
+    item.style.backgroundColor = "lightgray";
+}
+
+function unhighlightItem(item) {
+    item.style.backgroundColor = "";
+}
