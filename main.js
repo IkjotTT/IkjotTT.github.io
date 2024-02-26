f6788e6c310e106cecb68e5fe16e9b110963624b
IkjotTT <ikjot.titrie@torontomu.ca>
2024-02-26 16:46:44 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,10 @@
-// this is a function used to prompt the user for their name, the name is stored into name otherwise if nothing is entered, it is automatically set to guest 
+// this is a function used to prompt the user for their name, the name is stored into name variable and then displayed on screen on homepage
 function promptName() {
     var name = prompt("Please enter your name:");
     if (name != null && name != "") {
         document.getElementById("name").innerHTML = "Hello, " + name + "!";
         document.getElementById("nameButton").style.display = "none";
     }
-    else {
-        outputElement.innerHTML = "Hello, guest!";
-     }
 }
 
 function enlargeImage(img) { // a function that takes in the image, it togglest the class englarged which contains image resizing code
