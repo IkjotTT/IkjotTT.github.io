245b3dcf0bc441d3f3302ced05faddd1d47f826b
IkjotTT <ikjot.titrie@torontomu.ca>
2024-02-26 12:55:21 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,47 +1,20 @@
-function promptForName() {
+// this is a function used to prompt the user for their name, the name is stored into name otherwise if nothing is entered, it is automatically set to guest 
+function promptName() {
     var name = prompt("Please enter your name:");
     if (name != null && name != "") {
         document.getElementById("name").innerHTML = "Hello, " + name + "!";
         document.getElementById("nameButton").style.display = "none";
     }
-}
-
-// Task a: Create a button that prompts the user to type their name and display it in an HTML element
-
-function promptForName() {
-    var name = prompt("Please enter your name:");
-    var outputElement = document.getElementById("nameOutput");
-    if (name !== null && name !== "") {
-        outputElement.innerHTML = "Hello, " + name + "!";
-    } else {
+    else {
         outputElement.innerHTML = "Hello, guest!";
-    }
+     }
 }
 
-// Task b: Make a clickable image that enlarges when clicked
-
-function enlargeImage() {
-    var image = document.getElementById("myImage");
-    image.style.width = "200px"; // adjust the width as desired
-}
-
-// Task c: Create a customized navigation menu
-
-function toggleMenu() {
-    var menu = document.getElementById("menuItems");
-    if (menu.style.display === "block") {
-        menu.style.display = "none";
+var image = document.getElementById("myImage");
+image.addEventListener("click", function() {
+    if (image.width == 200) {
+        image.width = 400;
     } else {
-        menu.style.display = "block";
+        image.width = 200;
     }
-}
-
-// Task d: Add onmouseover/onmouseout events for navigation items
-
-function highlightItem(item) {
-    item.style.backgroundColor = "lightgray";
-}
-
-function unhighlightItem(item) {
-    item.style.backgroundColor = "";
-}
+});
